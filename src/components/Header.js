9f8d35a8a6310d079ffc14e4d39e4cd9b2c75dd2
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-15 10:07:23 +0530

    Header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,48 @@
+// import { Link } from "react-router-dom";
+// function Header()
+// {
+// return(
+// <div>
+ 
+// <Link to="Post">Post</Link><br></br>
+// <Link to="/Delete">Delete</Link><br></br>
+// <Link to="/Get">Get</Link><br></br>
+// <Link to="/contact">Contact Page</Link><br></br>
+// </div>
+// )
+// }
+// export default Header
+
+
+
+
+
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-function Header()
-{
-return(
-<div>
-<Link to="Post">Post</Link><br></br>
+import './Menu.css'; // Import CSS file for styling
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="menu-container">
+      <button className="menu-toggle" onClick={toggleMenu}>
+        Menu
+      </button>
+      <nav className={`menu ${isOpen ? 'open' : ''}`}>
+        <ul>
+        <Link to="Post">Post</Link><br></br>
 <Link to="/Delete">Delete</Link><br></br>
 <Link to="/Get">Get</Link><br></br>
 <Link to="/contact">Contact Page</Link><br></br>
-</div>
-)
+        </ul>
+      </nav>
+    </div>
+  );
 }
+
 export default Header
