d8c081c55a1a1ca7f525bf81fe5eb880b3ca5a8a
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-15 09:54:05 +0530

    vv

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+        <Navbar.Brand href="#home">HCL</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
