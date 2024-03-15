4b2f3e0ed39880d3ea1e0fa8f639b24d05c11da6
vijaymishra2303 <vijaymishra2303@gmail.com>
2024-03-15 10:01:32 +0530

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,13 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
-
-function Header() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">HCL</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="Post">Post</Nav.Link>
-            <Nav.Link href="Delete">Delete</Nav.Link>
-            <Nav.Link href="Get">Get</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+import { Link } from "react-router-dom";
+function Header()
+{
+return(
+<div>
+<Link to="Post">Post</Link><br></br>
+<Link to="/Delete">Delete</Link><br></br>
+<Link to="/Get">Get</Link><br></br>
+<Link to="/contact">Contact Page</Link><br></br>
+</div>
+)
 }
-
 export default Header
